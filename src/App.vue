35b5c892d88d8155ca45ed8cb88eb19558f26c98
ambitonyu <939649107@qq.com>
2020-02-21 14:18:52 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+#app{
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue'),
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('../views/Navigation.vue'),
+  },
+  {
+    path: '/changePassword',
+    name: 'home',
+    component: () => import('../views/ChangePassword.vue'),
+  },
+  {
+    path: '/windowCustoms',
+    name: 'windowCustoms',
+    component: () => import('../views/WindowCustoms.vue'),
+  },
+  {
+    path: '/myOrder',
+    name: 'myOrder',
+    component: () => import('../views/MyOrder.vue'),
+  },
+  {
+    path: '/myCustom',
+    name: 'myCustom',
+    component: () => import('../views/MyCustom.vue'),
+  },
+  {
+    path: '/bidding',
+    name: 'bidding',
+    component: () => import('../views/Bidding.vue'),
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
