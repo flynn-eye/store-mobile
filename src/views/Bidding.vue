3256439f707a93ecf8a17c4302411ec04b65fee5
ambitonyu <939649107@qq.com>
2020-02-21 14:19:43 +0800

    竞标

---
+++ src/views/Bidding.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <NavigationLayout
+        location-title='订单竞标'
+        :show-left='true'/>
+    </div>
+</template>
+
+<script>
+import NavigationLayout from '../components/layout/NavigationLayout.vue';
+
+export default {
+  name: 'Bidding',
+  components: {
+    NavigationLayout,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
