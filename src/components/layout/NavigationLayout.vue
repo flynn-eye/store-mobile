4d9efaf25313695dd5a4273ac6c6a0505014358d
ambitonyu <939649107@qq.com>
2020-02-21 14:18:30 +0800

    头部导航

---
+++ src/components/layout/NavigationLayout.vue
@@ -0,0 +1,61 @@
+<template>
+  <NavBar @click-left='back'
+          @click-right='rightClick'
+          :title='this.title'
+          :left-text='leftText'
+          :left-arrow='showLeft'>
+    <Icon :name='icon' slot="right"/>
+  </NavBar>
+</template>
+
+<script>
+import { NavBar, Icon } from 'vant';
+
+export default {
+  name: 'NavigationLayout',
+  components: {
+    NavBar,
+    Icon,
+  },
+  props: {
+    locationTitle: {
+      type: String,
+    },
+    showLeft: {
+      type: Boolean,
+      default: true,
+    },
+    icon: {
+      type: String,
+    },
+    rightClick: {
+      default: () => {},
+      type: Function,
+    },
+  },
+  created() {
+  },
+  methods: {
+    back() {
+      this.$router.back();
+    },
+  },
+  data() {
+    return {
+      title: this.locationTitle,
+    };
+  },
+  computed: {
+    leftText() {
+      if (this.showLeft === false) {
+        return '';
+      }
+      return '返回';
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
